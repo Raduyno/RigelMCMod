52cbf0626f15685d9121eb0c1917eee1e020cffd
Raduino <mimi03rtg@gmail.com>
2017-06-30 16:35:46 +0300

    Designing - AdminInfo

--- src/main/java/me/rigelmc/rigelmcmod/command/Command_admininfo.java
+++ src/main/java/me/rigelmc/rigelmcmod/command/Command_admininfo.java
@@ -14,13 +14,13 @@ public class Command_admininfo extends FreedomCommand
     @Override
     public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        msg("How to apply for admin on the RigelMCMod server:", ChatColor.AQUA);
-        msg(" - Do not ask for admin in game,", ChatColor.DARK_GREEN);
-        msg(" - Be helpful within the server,", ChatColor.GOLD);
-        msg(" - Report those breaking the rules,", ChatColor.DARK_GREEN);
-        msg(" - And apply on our forums at this link:", ChatColor.GOLD);
-        msg(" - http://forum.rigelmc.ga/forumdisplay.php?fid=13", ChatColor.DARK_AQUA);
-        msg(" - Do not apply for admin if you cannot be active!", ChatColor.RED);
+        msg("How to apply for admin on the RigelMCMod server:", ChatColor.YELLOW);
+        msg(" - Do not ask for admin in game,", ChatColor.AQUA);
+        msg(" - Be helpful within the server,", ChatColor.DARK_AQUA);
+        msg(" - Report those breaking the rules,", ChatColor.AQUA);
+        msg(" - And apply on our forums at this link:", ChatColor.DARK_AQUA);
+        msg(" - http://forum.rigelmc.ga/forumdisplay.php?fid=13", ChatColor.AQUA;
+        msg(" - Do not apply for admin if you cannot be active!", ChatColor.DARK_AQUA);
         return true;
     }
 }
